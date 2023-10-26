ab15d8bc35cf583b56e760d91e09a897db8dae04
SDH <personarpg7501@gmail.com>
2023-10-26 11:09:26 +0900

    npm install express 설치, get post 요청 예시 구현

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("hi");
+const express = require('express');
+
+
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/submit', (req, res) => {
+  const text = req.body.text;
+  console.log('입력된 텍스트:', text);
+  res.send('텍스트가 서버로 전송되었습니다.');
+});
+
+app.listen(port, () => {
+  console.log(`서버가 http://localhost:${port}에서 실행 중입니다.`);
+});
